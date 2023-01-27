c1c6c1663fc3c66324f714b69640918a08b15b4e
이은지 <eunjijjang11@naver.com>
2023-01-27 23:51:42 +0900

    Update Btn.tsx

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -24,14 +24,17 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+
   gap: 10px;
-  background-color: #f0f8ff;
+
+  background-color: white;
+  border-radius: 10px;
 `;
 
 const StyledImg = styled.img`
   width: 150px;
   height: 150px;
-  background-color: #f0f8ff;
+  background-color: white;
   @media (max-width: 800px) {
     width: 40px;
     height: 40px;
@@ -39,11 +42,10 @@ const StyledImg = styled.img`
 `;
 
 const StyledP = styled.p`
-  background-color: #f0f8ff;
-
+  background-color: white;
+  font-weight: 650;
   @media (max-width: 800px) {
     margin: 10px;
-
     text-align: center;
     font-size: 14px;
   }
@@ -54,15 +56,15 @@ const StyledBtn = styled.button`
   border: 3px solid #007acc;
   border-radius: 1rem;
   color: black;
-  background-color: #f0f8ff;
-
+  font-weight: 550;
+  background-color: white;
   cursor: pointer;
 
   transition: 0.2s;
 
   &:hover {
     transform: translateY(-2px);
-    color: #f0f8ff;
+    color: white;
     background-color: #007acc;
   }
 
