2f2873aebe97913ffcb594fb4515fcfb8fed47a1
이은지 <eunjijjang11@naver.com>
2023-01-28 04:59:00 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api/samsung',
+        destination: `http://want2u.duckdns.org/main/1`,
+      },
+      {
+        source: '/api/apple',
+        destination: `http://want2u.duckdns.org/main/2`,
+      },
+      {
+        source: '/api/lg',
+        destination: `http://want2u.duckdns.org/main/3`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
