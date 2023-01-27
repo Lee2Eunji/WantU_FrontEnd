4f43fccd5f726b0b0b8e89d0e079ede058c868df
이은지 <eunjijjang11@naver.com>
2023-01-27 23:29:10 +0900

    Update detail.tsx

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -7,24 +7,35 @@ export default function Detail() {
       <Nav />
       <Container>
         <StyledH1>(제품 이름)</StyledH1>
-        <Collect>
-          <Category>
-            <StyledSpan>기관 명</StyledSpan>
-            <StyledSpan>가격</StyledSpan>
-          </Category>
-          <Category>
-            <StyledSpan>학생복지스토어</StyledSpan>
-            <StyledSpan>(제품 가격)</StyledSpan>
-          </Category>
-          <Category>
-            <StyledSpan>교육할인스토어</StyledSpan>
-            <StyledSpan>(제품 가격)</StyledSpan>
-          </Category>
-          <Category>
-            <StyledSpan>공식홈</StyledSpan>
-            <StyledSpan>(제품 가격)</StyledSpan>
-          </Category>
-        </Collect>
+        <StyledDiv>
+          <Collect>
+            <Category>
+              <StyledSpan>기관 명</StyledSpan>
+              <StyledSpan>가격</StyledSpan>
+            </Category>
+            <Category>
+              <StyledSpan>학생복지스토어</StyledSpan>
+              <StyledSpan>(제품 가격)</StyledSpan>
+            </Category>
+            <Category>
+              <StyledSpan>교육할인스토어</StyledSpan>
+              <StyledSpan>(제품 가격)</StyledSpan>
+            </Category>
+            <Category>
+              <StyledSpan>공식홈</StyledSpan>
+              <StyledSpan>(제품 가격)</StyledSpan>
+            </Category>
+          </Collect>
+          <StyledImageWrapper>
+            <StyledImg
+              src="./g_book2.webp"
+              alt=""
+              style={{ display: 'flex', justifyContent: 'center' }}
+            />
+            <StyledP>사이즈: 14인치</StyledP>
+            <StyledP>무게: 1kg</StyledP>
+          </StyledImageWrapper>
+        </StyledDiv>
       </Container>
     </>
   );
@@ -37,35 +48,41 @@ const Collect = styled.div`
   grid-template-rows: 5vw 8vw 8vw 8vw;
 
   margin-bottom: 2rem;
-  margin-left: 2rem;
+  margin-left: 10rem;
 
   @media (max-width: 800px) {
     margin-top: 5rem;
+    margin-left: 4rem;
     grid-template-columns: 50vw;
     grid-template-rows: 5vw 8vw 8vw 8vw;
   }
 `;
 
 const Container = styled.div`
-background-color: black;
+  background-color: black;
 `;
 
 const StyledH1 = styled.h1`
   padding: 3% 0 0 0;
-  margin-left: 4rem;
+  margin-left: 12rem;
 
   background-color: black;
   color: #d3d3d3;
 
   @media (max-width: 800px) {
     margin-top: 2rem;
+    margin-left: 6rem;
   }
 `;
 
+const StyledDiv = styled.div`
+  display: flex;
+`;
+
 const Category = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   margin: 0 2rem;
   padding: 0 1rem;
@@ -74,4 +91,37 @@ align-items: center;
 
 const StyledSpan = styled.span`
   color: #d3d3d3;
+`;
+
+const StyledImg = styled.img`
+  width: 20vw;
+  height: 30vh;
+
+  margin-bottom: 2rem;
+
+  @media (max-width: 800px) {
+    width: 20vw;
+    height: 20vh;
+    margin-top: 5rem;
+    margin-right: 4rem;
+    margin-bottom: 0;
+  }
+`;
+
+const StyledImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const StyledP = styled.p`
+  display: flex;
+
+  color: white;
+
+  @media (max-width: 800px) {
+    margin-right: 4rem;
+  }
 `;
