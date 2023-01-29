1951ff6752b447c98c5537d8d0a498f01b478b1b
이은지 <eunjijjang11@naver.com>
2023-01-29 23:39:54 +0900

    Update s_list.tsx

--- src/pages/s_list.tsx
+++ src/pages/s_list.tsx
@@ -5,32 +5,24 @@ import List from '../components/List';
 import Nav from '../components/Nav';
 
 interface Item {
-  id: number,
-  name: string,
-  maker_id: number,
-  price: number,
-  link: string,
+  id: number;
+  name: string;
+  maker_id: number;
+  price: number;
+  link: string;
 }
 
 const S_list = () => {
-
-  useEffect(() => {
-    const getapi = async () => {
-      const getdata = await axios.get('/api/samsung').then((res) => console.log(res));
-    }
-    getapi();
-  }, [])
-
   return (
     <div>
       <Nav />
       <StyledDiv>
         <StyledH1>SAMSUNG</StyledH1>
-        <List />
+        <List typeofList={1} />
       </StyledDiv>
     </div>
   );
-}
+};
 
 export default S_list;
 
